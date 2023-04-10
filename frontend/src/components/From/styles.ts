9845ece27d3ee1126eaed88d2207c515fa378133
Bruno Lessa <brunolessa@live.com>
2023-04-10 17:00:45 -0300

    style from user container

--- frontend/src/components/From/styles.ts
+++ frontend/src/components/From/styles.ts
@@ -4,7 +4,12 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   width: 50em;
-  padding: 1em;
+  padding: 2em 1em 1em 1em;
   border-radius: 0.3em;
   background: #f0f0f5;
+
+  & > figure {
+    text-align: center;
+    margin-bottom: 1em;
+  }
 `;
