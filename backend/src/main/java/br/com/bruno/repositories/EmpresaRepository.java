4065d433ec1fe23bb8ea22a7fa47115aaf029051
Bruno Lessa <brunolessa@live.com>
2023-04-10 15:11:40 -0300

    create repositories

---
+++ backend/src/main/java/br/com/bruno/repositories/EmpresaRepository.java
@@ -0,0 +1,9 @@
+package br.com.bruno.repositories;
+
+import br.com.bruno.entities.Empresa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+}
---
+++ backend/src/main/java/br/com/bruno/repositories/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package br.com.bruno.repositories;
+
+import br.com.bruno.entities.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
