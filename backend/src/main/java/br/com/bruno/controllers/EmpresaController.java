7f69a3f4c9071c0ec660e8a9a98497e79992faf2
Bruno Lessa <brunolessa@live.com>
2023-04-10 16:03:01 -0300

    enpoint list all paged empresas

---
+++ backend/src/main/java/br/com/bruno/controllers/EmpresaController.java
@@ -0,0 +1,26 @@
+package br.com.bruno.controllers;
+
+import br.com.bruno.dtos.EmpresaDTO;
+import br.com.bruno.services.EmpresaService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/empresas")
+@RequiredArgsConstructor
+@Slf4j
+public class EmpresaController {
+
+    private final EmpresaService empresaService;
+
+    @GetMapping
+    public ResponseEntity<Page<EmpresaDTO>> findAll(Pageable pageable) {
+        return ResponseEntity.ok(empresaService.findAll(pageable));
+    }
+}
